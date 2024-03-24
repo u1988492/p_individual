60b460b20e54385d01fef01d00c09c8d55fd8b0a
Claudia <u1988492@campus.udg.edu>
2024-03-24 13:53:42 +0100

    added php files

--- php/save.php
+++ php/save.php
@@ -10,7 +10,7 @@
 
     $encodeCards = json_encode($_SESSION['cards']);
 
-    $conn = oci_connect('u4000775', '*********', 'ORCLCDB');
+    $conn = oci_connect('u1988492', 'Mlogrono24.', 'ORCLCDB');
     $insert="INSERT INTO memory_save
     (uuid, pairs, points, cards )
     VALUES
